023e5aabc90fa20ce21f50c07880c5106128a2ca
Otávio Prado <otavio.prado@sensedia.com>
2019-06-04 21:29:42 -0300

    Adicionando @CrossOrigin para o front consumir a API

--- src/main/java/com/sensedia/apix2019/kit/controller/KitController.java
+++ src/main/java/com/sensedia/apix2019/kit/controller/KitController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.sensedia.apix2019.kit.service.KitService;
 
 @RestController
 @RequestMapping("/kits")
+@CrossOrigin
 public class KitController {
 
     @Autowired
